903509d9a046b5c0b888cf87107c35b426feda3b
Tomiloba21 <olowo15tomiloba@gmail.com>
2024-06-04 22:29:46 +0100

    first commit

---
+++ src/main/java/com/tomiloba/runners/run/RunNotFoundException.java
@@ -0,0 +1,10 @@
+package com.tomiloba.runners.run;/**
+*@project runners
+*
+*
+*@Author <h3 style="color: green; padding: 0px;"> Tomiloba</h3>
+*
+*
+*@since 6/4/2024
+*/public class RunNotFoundException {
+}
