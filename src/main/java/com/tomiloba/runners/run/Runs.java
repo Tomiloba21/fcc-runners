55526bb1869c08436abc5a3b403029438eb3b8c5
Tomiloba21 <olowo15tomiloba@gmail.com>
2024-06-06 09:33:11 +0100

    finalized using the h2-embedded db

---
+++ src/main/java/com/tomiloba/runners/run/Runs.java
@@ -0,0 +1,11 @@
+package com.tomiloba.runners.run;
+
+import java.util.List;
+
+/**
+ * @project runners
+ * @Author <h3 style="color: green; padding: 0px;"> Tomiloba</h3>
+ * @since 6/6/2024
+ */
+public record Runs(List<Run> runs) {
+}
